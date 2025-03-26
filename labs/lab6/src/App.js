d77cabbb16374dac69fbb3bbb50846335fa3ddd1
Yuthmin <mn847782@dal.ca>
2025-03-26 19:00:45 -0300

    corrected import paths

--- labs/lab6/src/App.js
+++ labs/lab6/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from '../../node_modules/react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
