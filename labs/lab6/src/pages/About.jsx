e8347d73f08f602fc0353cd9808c2887a20f9d8d
Yuthmin <mn847782@dal.ca>
2025-04-06 20:30:53 -0300

    test images

--- labs/lab6/src/pages/About.jsx
+++ labs/lab6/src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <main>
       <section>
         <h1>About Me</h1>
-        <img src="/public/profilePic.jpg" alt="Your Profile" className="profile-img" />
+        <img src="/profilePic.jpg" alt="Your Profile" className="profile-img" />
         <p>Hi, I'm Manuga Yuthmin Wijesundara Bandara, a third-year Computer Science student at Dalhousie University with a passion for AI and networks. I'm particularly interested in exploring how intelligent systems can optimize network performance and solve real-world challenges. Beyond academics, I love working out and have recently taken up running as a new challenge. I'm always excited to learn, build, and collaborate on innovative tech projects!</p>
       </section>
     </main>
