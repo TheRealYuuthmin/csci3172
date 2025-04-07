2884377b99c88e75a681fe15042b55f7f2c5cabb
Yuthmin <mn847782@dal.ca>
2025-04-06 21:32:38 -0300

    simplified projeccts function data loading

--- labs/lab6/netlify/functions/projects.js
+++ labs/lab6/netlify/functions/projects.js
@@ -1,13 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-
 exports.handler = async (event, context) => {
-  const projectsFilePath = path.resolve(__dirname, '../../backend/projects.json'); 
-
   try {
 
-    const data = fs.readFileSync(projectsFilePath, 'utf8');
-    const jsonData = JSON.parse(data);
+    const jsonData = require('./_data/projects.json');
+
 
     return {
       statusCode: 200,
@@ -21,7 +16,7 @@ exports.handler = async (event, context) => {
     console.error('Error processing projects function:', error);
     return {
       statusCode: 500,
-       headers: {
+      headers: {
         'Access-Control-Allow-Origin': '*', 
       },
       body: JSON.stringify({ error: 'Failed to load project data.' }),
